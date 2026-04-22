b8f69ded039d198f75f1b46c136a611e03120b13
Mike Duigou <bondolo@github.com>
2026-04-22 11:46:38 -0700

    update to junit 6.0.3

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
     api("org.jspecify:jspecify:1.0.0")
     api("org.joml:joml:1.10.8")
 
-    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation(platform("org.junit:junit-bom:6.0.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.junit.jupiter:junit-jupiter-params")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
