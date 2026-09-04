2798609661c9aaa644e2625bdc014c036baf976b
Mike Duigou <bondolo@github.com>
2026-09-04 16:55:38 -0700

    remove dead code

--- base/src/main/java/com/oddlabs/tt/base/util/FileListerInterface.java
+++
@@ -1,7 +0,0 @@
-package com.oddlabs.tt.base.util;
-
-import java.io.File;
-
-public interface FileListerInterface {
-    void newFiles(File[] file);
-}
--- base/src/main/java/com/oddlabs/tt/base/util/FileListerListener.java
+++
@@ -1,7 +0,0 @@
-package com.oddlabs.tt.base.util;
-
-import java.io.File;
-
-public interface FileListerListener {
-    void newFiles(File[] files);
-}
