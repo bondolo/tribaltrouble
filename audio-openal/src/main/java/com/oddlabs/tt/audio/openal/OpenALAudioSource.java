f68361f21d08e5c36df5f63c997aa489493a5f68
Mike Duigou <bondolo@github.com>
2026-09-19 13:04:18 -0700

    fix another AL_INVALID_OPERATION error source had a buffer from prior use

--- audio-openal/src/main/java/com/oddlabs/tt/audio/openal/OpenALAudioSource.java
+++ audio-openal/src/main/java/com/oddlabs/tt/audio/openal/OpenALAudioSource.java
@@ -183,6 +183,10 @@ final class OpenALAudioSource extends NativeResource<OpenALAudioSource.Source> i
         int sourceId = getSource();
         if (AL10.alIsSource(sourceId)) {
             assert al_buffers.remaining() > 0 : "al_buffers is empty";
+            if (AL10.alGetSourcei(sourceId, AL10.AL_BUFFER) != 0) {
+                AL10.alSourcei(sourceId, AL10.AL_BUFFER, AL10.AL_NONE);
+                checkALError("alSourcei AL_BUFFER AL_NONE before queue");
+            }
             AL10.alSourceQueueBuffers(sourceId, al_buffers);
             checkALError("alSourceQueueBuffers");
         }
